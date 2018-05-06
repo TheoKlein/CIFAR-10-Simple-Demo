865d04ca8aef42316499ac80cddc3058dda6ed23
TheoKlein <theokleintw@gmail.com>
2018-05-06 22:31:54 +0800

    update cnn.py

--- cnn.py
+++ cnn.py
@@ -10,16 +10,18 @@ from tflearn.layers.conv import conv_2d, max_pool_2d
 from tflearn.layers.estimator import regression
 
 LEARNING_RATE = 0.001
-EPOCH = 10
+EPOCH = 100
 NAME = 'cifar10_cnn_ep{}'.format(EPOCH)
 
 # Define Convolutional Neural Network structure
 network = input_data(shape=[None, 32, 32, 3], name="input")
+
 network = conv_2d(network, 32, 3, activation='relu')
 network = max_pool_2d(network, 2)
-network = conv_2d(network, 64, 3, activation='relu')
+
 network = conv_2d(network, 64, 3, activation='relu')
 network = max_pool_2d(network, 2)
+
 network = fully_connected(network, 512, activation='relu')
 network = dropout(network, 0.5)
 network = fully_connected(network, 10, activation='softmax')
